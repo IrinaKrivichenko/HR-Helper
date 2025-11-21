6ac6edf766123820d9e00e440c5febbd4ee176db
irina <irina.krivichenko@gmail.com>
2025-11-22 00:02:39 +0300

    telegramm bot test2

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 
-from src.bot.utils import send_message
 
 load_dotenv()
 
@@ -14,7 +13,7 @@ async def handle_forwarded(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
     user_name = user.username if user.username else user.first_name
     text = update.message.text
-    await send_message(update, f"Hi, {user_name}. Your have sent me {text}")
+    await update.message.reply_text(f"Hi, {user_name}. Your have sent me {text}")
 
 
 
